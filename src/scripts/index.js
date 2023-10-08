3ab373856fc6962eb3b783f088bad7d068bec888
Binatik <sadxas999@mail.ru>
2023-10-08 20:37:06 +0300

    изменение архитектуры  scss

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,11 +1,4 @@
 import '../scss/utils/fonts.scss'
 import '../scss/global.scss'
 
-import { header } from "./modules";
-import { title, link } from "./ui";
 
-console.log("Test complited Webpack");
-
-header()
-title()
-link()
