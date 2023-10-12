ac92aa3bc291b53aaee8a76cec735f60970c0c92
Binatik <sadxas999@mail.ru>
2023-10-12 22:43:48 +0300

    переработана шапка с меню

--- src/scripts/modules/header/header.js
+++ src/scripts/modules/header/header.js
@@ -1,29 +1,32 @@
 function header() {
-  const textAttr = '[data-menu="off"]';
-  const trigger = document.querySelector(textAttr);
+  const menu = document.querySelector(".header__menu");
+  const blure = document.querySelector('.blure')
+  const openMenu = document.querySelector("#open-menu");
+  const closeMenu = document.querySelector("#close-menu");
+  const classMenuOn = "menu--on";
+  const classMenuOff = "menu--off";
+  const classBlureOn = 'blure--on'
+  const classBlureOff = 'blure--off'
 
-  trigger.addEventListener("click", () => {
-    const headerRight = document.querySelector(".header__right");
-    const menu = document.querySelector(".header__menu");
-    const headerSearch = document.querySelector(".header__search");
-    const classMenuOn = "menu--on";
-    const classMenuOff = "menu--off";
-    const hidden = "hidden";
-
-    trigger.dataset.menu = trigger.dataset.menu === "on" ? "off" : "on";
-
-    if (trigger.dataset.menu === "off") {
-      menu.classList.remove(classMenuOn);
-      menu.classList.add(classMenuOff);
-      headerRight.classList.remove(hidden);
-      headerSearch.classList.add(hidden);
-      return;
-    }
-
+  openMenu.addEventListener("click", () => {
     menu.classList.add(classMenuOn);
     menu.classList.remove(classMenuOff);
-    headerRight.classList.add(hidden);
-    headerSearch.classList.remove(hidden);
+    blure.classList.add(classBlureOn)
+    blure.classList.remove(classBlureOff)
+  });
+
+  closeMenu.addEventListener("click", () => {
+    menu.classList.remove(classMenuOn);
+    menu.classList.add(classMenuOff);
+    blure.classList.add(classBlureOff)
+    blure.classList.remove(classBlureOn)
+  });
+
+  blure.addEventListener("click", () => {
+    menu.classList.remove(classMenuOn);
+    menu.classList.add(classMenuOff);
+    blure.classList.add(classBlureOff)
+    blure.classList.remove(classBlureOn)
   });
 }
 
