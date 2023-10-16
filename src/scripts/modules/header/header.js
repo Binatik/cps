6a6d1b453d1cc33e020a8c6fa4462c7149d2dbdc
Binatik <sadxas999@mail.ru>
2023-10-16 18:23:52 +0300

    фиксы меню, работа над main

--- src/scripts/modules/header/header.js
+++ src/scripts/modules/header/header.js
@@ -1,32 +1,41 @@
 function header() {
   const menu = document.querySelector(".header__menu");
-  const blure = document.querySelector('.blure')
+  const blure = document.querySelector(".blure");
   const openMenu = document.querySelector("#open-menu");
   const closeMenu = document.querySelector("#close-menu");
   const classMenuOn = "menu--on";
   const classMenuOff = "menu--off";
-  const classBlureOn = 'blure--on'
-  const classBlureOff = 'blure--off'
+  const classBlureOn = "blure--on";
+  const classBlureOff = "blure--off";
+  const desktopWidth = 1440;
+  const width = document.body.clientWidth;
+  
+  if (width > desktopWidth) {
+    menu.classList.add(classMenuOn);
+    menu.classList.remove(classMenuOff);
+    blure.classList.add(classBlureOff);
+    blure.classList.remove(classBlureOn);
+  }
 
   openMenu.addEventListener("click", () => {
     menu.classList.add(classMenuOn);
     menu.classList.remove(classMenuOff);
-    blure.classList.add(classBlureOn)
-    blure.classList.remove(classBlureOff)
+    blure.classList.add(classBlureOn);
+    blure.classList.remove(classBlureOff);
   });
 
   closeMenu.addEventListener("click", () => {
     menu.classList.remove(classMenuOn);
     menu.classList.add(classMenuOff);
-    blure.classList.add(classBlureOff)
-    blure.classList.remove(classBlureOn)
+    blure.classList.add(classBlureOff);
+    blure.classList.remove(classBlureOn);
   });
 
   blure.addEventListener("click", () => {
     menu.classList.remove(classMenuOn);
     menu.classList.add(classMenuOff);
-    blure.classList.add(classBlureOff)
-    blure.classList.remove(classBlureOn)
+    blure.classList.add(classBlureOff);
+    blure.classList.remove(classBlureOn);
   });
 }
 
